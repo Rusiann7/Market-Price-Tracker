04d696e4fc9f77dffcb8b7bd1aa98021400256f1
Rusiann <Rusiann7@protonmail.com>
2025-04-27 18:22:54 +0800

    fixed some stuff

--- src/Hello_World.vue
+++ src/Hello_World.vue
@@ -4,7 +4,7 @@
             <h1>Copyrighted by Rusiann, Rhic</h1>
             <h2>Other codes used in this project, go to their respective creators. Thanks</h2>
             <p>Special thanks to the A.I. that debugged this entire codebase of a mess and shout out to the school.</p>
-            <p>This is 2B she is our maskcot</p>
+            <p>This is 2B she is our mascot</p>
             <img src="@/assets/overlay1.png" alt="yes" style="max-width: 400px; width: 100%; height: auto; display: block; margin: 0 auto;">
         </div>
     </div>
